da3dc9b1e047fdda281e942975424a55938096be
Warren Leyes <warren@leyes.ca>
2016-07-26 15:25:43 -0700

    added sass/compass to gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
+    compass = require('gulp-compass'),
     browserify = require('gulp-browserify'),
     coffee = require('gulp-coffee'),
     concat = require('gulp-concat');
@@ -25,4 +26,18 @@ gulp.task('js', function () {
        .pipe(concat('script.js'))
        .pipe(browserify())
        .pipe(gulp.dest('builds/development/js'))
+});
+
+
+
+var sassSources = ['components/sass/style.scss'];
+gulp.task('compass', function () {
+    gulp.src(sassSources)
+        .pipe(compass({
+            sass: 'components/sass',
+            image: 'builds/development/images',
+            style: 'expanded'
+        }))
+        .on('error', gutil.log)
+        .pipe(gulp.dest('builds/development/css'))
 });
