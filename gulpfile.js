eb8a65cc2c93e1a6a64abe6326ebdfc0a84f6c57
Warren Leyes <warren@leyes.ca>
2016-07-26 15:07:49 -0700

    added Browserify

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,10 @@
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
+    browserify = require('gulp-browserify'),
     coffee = require('gulp-coffee'),
     concat = require('gulp-concat');
 
 var coffeeSources = ['components/coffee/*.coffee'];
-
 gulp.task('coffee', function () {
     gulp.src(coffeeSources)
         .pipe(coffee({bare: true})
@@ -13,15 +13,16 @@ gulp.task('coffee', function () {
 });
 
 
+
 var jsSources =[
     'components/scripts/*.js',
     'components/scripts/pixgrid.js',
     'components/scripts/tagline.js',
     'components/scripts/template.js'
 ];
-
 gulp.task('js', function () {
    gulp.src(jsSources)
        .pipe(concat('script.js'))
+       .pipe(browserify())
        .pipe(gulp.dest('builds/development/js'))
 });
