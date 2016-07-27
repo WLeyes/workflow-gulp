a164a6ff6dfde34c01e4891067f0ccb0eddda7af
Warren Leyes <warren@leyes.ca>
2016-07-26 17:27:05 -0700

    added watch task

--- gulpfile.js
+++ gulpfile.js
@@ -41,3 +41,15 @@ gulp.task('compass', function () {
         .on('error', gutil.log)
         .pipe(gulp.dest('builds/development/css'))
 });
+
+
+
+gulp.task('default', ['coffee', 'js', 'compass']);
+
+
+
+gulp.task('watch', function () {
+    gulp.watch(coffeeSources, ['coffee']);
+    gulp.watch(jsSources, ['js']);
+    gulp.watch('components/sass/*.scss', ['compass']);
+});
